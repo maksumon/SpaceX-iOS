5a716e169c302deae7125387c5378ba50dc2a428
Mohammad Ashraful Kabir <maksumon007@gmail.com>
2022-01-30 12:33:07 +0800

    Created string extension to format ISO date

--- SpaceX/Extensions/StringExtension.swift
+++ SpaceX/Extensions/StringExtension.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension String {
+    func formatISODateTime() -> String {
+        var formattedDateTime = self
+        let isoDateFormatter = ISO8601DateFormatter()
+        isoDateFormatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
+        let date = isoDateFormatter.date(from: self)
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+
+        if let formattedDate = date {
+            formattedDateTime = dateFormatter.string(from: formattedDate)
+        } else {
+           print("There was an error formatting the string to date")
+        }
+        
+        return formattedDateTime
+    }
+}
